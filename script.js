42acce0fac0c9a0c4d2898272e5d136114d7fc76
yuvrajsainiYS <duhariyayuvraj@gmail.com>
2024-09-09 05:57:37 -0700

    workin and

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let container = document.getElementsByClassName("container")[0]
 let active = document.getElementById("active")[0]
  
 function  act(){
-    container.classList.toogle("active")
+    container.classList.toggle("active")
 }
